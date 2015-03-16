ba4a949d1b926019f93c01ab9f982d662f76a593
Jesse Nagelberg <jnagelberg@gmail.com>
2015-03-16 17:17:38 -0400

    shit is finally working, kind of

--- cookie_monster/app/assets/javascripts/posts.js
+++ cookie_monster/app/assets/javascripts/posts.js
@@ -1,220 +1,24 @@
-// // GOOGLE MAPS API
-
-// var map;
-// var service;
-
-// // add search request to include places that are only 'openNow'
-// function handleSearchResults(results, status){
-//   console.log(results);
-
-//   for (var i = 0; i < results.length; i++) {
-//     var marker = new google.maps.Marker({
-//       position: results[i].geometry.location,
-//       map: map,
-//       icon: 'http://i.imgur.com/DfA8TrB.png?1' 
-//     });
-//   }
-// }
-
-// function performSearch(){
-//   var request = {
-//     bounds: map.getBounds(),
-//     keyword: 'cookies'
-//   }
-//   service.nearbySearch(request, handleSearchResults);
-// }
-
-// function initialize(location) {
-//   console.log(location);
-//   // debugger;
-//   // lat = location.coords.latitude;
-//   // lng = location.coords.longitude;
-
-//   var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
-//   var mapOptions = {
-//     center: currentLocation,
-//     zoom: 14,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
-
-//   map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-//   var marker = new google.maps.Marker({
-//     position: currentLocation,
-//     map: map,
-//     icon:'http://i.imgur.com/rmrs7kG.png?1'
-//   });
-
-//   service = new google.maps.places.PlacesService(map);
-
-//   // this ensures that we wait until the map bounds are initialized before we perform the search
-//   google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
-// }
-
-//   // var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-//   // google.maps.event.addListener(marker,'click',function(e){
-
-//   //   infoWindow.open(map, marker);
-
-//   // });
-
-// // $(document).ready(function(){
-// //   navigator.geolocation.getCurrentPosition(initialize);
-// // });
-
-
-// // FOURSQUARE API
-
-// $(document).ready(function(){
-//   var lat, lng;
-//   var returnCoords = function(position){
-//     console.log("callback");
-//     lat = position.coords.latitude;
-//     lng = position.coords.longitude;
-//   }
-//   var somethingWentWrong = function(error){
-//     console.log("callback");
-//   }
-//   navigator.geolocation.getCurrentPosition(getVenues, somethingWentWrong)
-//   navigator.geolocation.getCurrentPosition(initialize);
-//   // debugger;
-//   // if (!lat) {
-//   // } else {
-//   //   getVenues();
-//   // }
-//   //
-//   // var keyword = "cookies"
-
-//   // getVenues( keyword );
-// });
-
-// // get lat & lng from google to put in url for ajax
-
-
-// function getVenues(location) {
-//   // function getLocation(location) {
-//   // debugger;
-//     var keyword = "cookies";
-//     var lat = location.coords.latitude;
-//     var lng = location.coords.longitude;
-
-//     var data = {keyword: keyword, lat: lat, lng: lng};
-
-//   // var coords = navigator.geolocation.getCurrentPosition(initialize);
-
-//     // $.ajax({
-//     //   url: "https://api.foursquare.com/v2/venues/explore?ll=" + lat + "," + lng + " &client_id=I34KOKW5DI2MSD12IHDY2KKYIT2UDUD2VHDC5WTBWX4TG4SJ&client_secret=1V0VGFUVO10EJ0NL03EJCIDNLVS5FTZC0WWWCSXAWY5UDHF2&v=20150301&query="+keyword,
-//     //   type: 'GET',
-//     //   dataType: 'json',
-//     //   success: function (data) {
-//     //     console.log(data);
-
-//     $.ajax({
-//       url: '/venues',
-//       type: 'Get',
-//       dataType: 'json',
-//       data: data,
-//       success: function (data) {
-//         console.log(data);
-
-//       $('#venues').show();
-//         var dataObj = data.response.groups[0].items;
-//         $('#venues').empty();
-
-//         // storeVenues(dataObj);
-
-//         // Build markers and elements from venues returned
-//       $.each( dataObj, function() {
-//         if (this.venue.categories[0].name) {
-//           category = "Category: " + this.venue.categories[0].name;
-//         } else {
-//           category = "";
-//         }
-
-//         if (this.venue.location.formattedAddress) {
-//           address = "Address: " + this.venue.location.formattedAddress;
-//         } else {
-//           address = "";
-//         }
-
-//         if (this.venue.contact.formattedPhone) {
-//           phone = "Phone: " + this.venue.contact.formattedPhone;
-//         } else {
-//           phone = "";
-//         }
-
-//         // if (this.venue.menu.url) {
-//         //   menu = "Menu URL: " + this.venue.menu.url;
-//         // } else {
-//         //   menu = "";
-//         // }
-
-//         // if (this.venue.price.message != undefined) {
-//         //   pricing = "Pricing: " + this.venue.price.message;
-//         // } else {
-//         //   pricing = "";
-//         // }
-
-//         if (this.venue.location.distance) {
-//           distance = "Distance: " + (this.venue.location.distance / 1609.3).toPrecision(2) + " miles";
-//         } else {
-//           distance = "";
-//         }
-
-//         if (this.venue.rating) {
-//           rating = "Rating: " + this.venue.rating;
-//         } else {
-//           rating = "";
-//         }
-
-//         if (this.venue.url) {
-//           website = "Website: " + this.venue.url;
-//         } else {
-//           website = "";
-//         }
-
-//       var appendDataHtml = '<div id="venues"><h2><span>' + this.venue.name + '</span></h2>' + category + '<br>' + address + '<br>' + phone + '<br>' + distance + '<br>' + rating + '<br>' + website + '</div>';
-//       $('#venues').append(appendDataHtml);
-//         });
-//       },
-//     });
-//   }
-
-////////////////////// FOURSQUARE API ////////////////////
-var dataobj;
-var baseUrl = "https://api.foursquare.com/v2/venues/explore?ll=";
-var secondUrl = "&venuePhotos=1&client_id=I34KOKW5DI2MSD12IHDY2KKYIT2UDUD2VHDC5WTBWX4TG4SJ&client_secret=1V0VGFUVO10EJ0NL03EJCIDNLVS5FTZC0WWWCSXAWY5UDHF2&v=20150301&query=";
-
-////////////////// GOOGLE MAPS API MARKERS //////////////////////
-
+var venues;
 var map;
-var service;
 var markers = [];
 
-/////////// USER'S Location on Google Maps ///////////////////
+function renderMap(location) {
 
-function userLocation(location) {
-  console.log(location);
-
-  var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
-  var mapOptions = {
-    center: currentLocation,
-    zoom: 14,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+    var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+    var mapOptions = {
+      center: currentLocation,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  var marker = new google.maps.Marker({
-   position: currentLocation,
-   map: map,
-   animation: google.maps.Animation.DROP,
-   icon: 'http://i.imgur.com/DfA8TrB.png?1'
- });
-
-  service = new google.maps.places.PlacesService(map);
-
-  setMarkers(map, dataobj);
+    var marker = new google.maps.Marker({
+     position: currentLocation,
+     map: map,
+     animation: google.maps.Animation.DROP,
+     icon: 'http://i.imgur.com/DfA8TrB.png?1'
+   });
 }
 
 function toggleBounce(){
@@ -225,21 +29,18 @@ function toggleBounce(){
   }
 }
 
-//////////////////// ON PAGE LOAD /////////////////////////
-
 $(document).ready(function(){
   var lat, lng;
+
   var returnCoords = function(position){
-    console.log("callback");
     lat = position.coords.latitude;
     lng = position.coords.longitude;
   };
-  var somethingWentWrong = function(error){
-    console.log("callback");
-  };
-  navigator.geolocation.getCurrentPosition(getVenues, somethingWentWrong);
-  navigator.geolocation.getCurrentPosition(userLocation);
-  // navigator.geolocation.getCurrentPosition(setMarkers);
+
+  navigator.geolocation.getCurrentPosition(getVenues);
+  navigator.geolocation.getCurrentPosition(renderMap);
+
+});
 
   function getVenues(location) {
 
@@ -248,18 +49,35 @@ $(document).ready(function(){
     var lng = location.coords.longitude;
 
     $.ajax({
-      url: baseUrl + lat + "," + lng + secondUrl + keyword,
+      url: "https://api.foursquare.com/v2/venues/explore?ll=" + lat + "," + lng + "&venuePhotos=1&client_id=I34KOKW5DI2MSD12IHDY2KKYIT2UDUD2VHDC5WTBWX4TG4SJ&client_secret=1V0VGFUVO10EJ0NL03EJCIDNLVS5FTZC0WWWCSXAWY5UDHF2&v=20150301&query=" + keyword,
       type: 'GET',
       dataType: 'json',
       success: function (data) {
-        console.log(data);
-
         $('#venues').show();
         venues = data.response.groups[0].items;
         $('#venues').empty();
 
         // Builds markers and elements from venues returned
         $.each( venues, function() {
+          // TODO :: STORE THINGS IN markers
+          var venueLat = this.venue.location.lat;
+          var venueLng = this.venue.location.lng;
+          var newMarker = venueLat + venueLng;
+          // var contentString = '<div id="infowindow-content">' +
+          // '<h3>' + this.venue.name + '</h3>' +
+          // '<p>Address: ' + this.venue.location.formattedAddress + '</p>' +
+          // '<p>Category: ' + this.venue.categories[0].name + '</p>' +
+          // '<p>Rating: ' + this.venue.rating + '</p>' +
+          // '</div>';
+
+          // var infowindow = new google.maps.InfoWindow({
+          //   content: contentString
+          // });
+
+          // attachInfo(infowindow, map, newMarker);
+
+          markers.push( newMarker );
+
           if (this.venue.categories[0].name) {
             category = "Category: " + this.venue.categories[0].name;
           } else {
@@ -297,7 +115,6 @@ $(document).ready(function(){
           }
 
           var $venuesList = $('#venues');
-          // var $venueListCoords = $('#venues').push(venueCoordinates);
           var $newVenueLi = $('<li><div class="venue-name"><h2>' + this.venue.name + '</h2></div></li>' );
 
           var $newVenueShow = $('<div class="venue-show"></div>');
@@ -307,7 +124,6 @@ $(document).ready(function(){
           $newVenueShow.append('<p>' + distance + '</p>');
           $newVenueShow.append('<p>' + rating + '</p>');
           $newVenueShow.append('<p>' + website + '</p>');
-          // $newVenueShow.append('<p>' + photo + '</p>');
 
           var $newVenueCommentsList = $('<ul class="comments-list"></ul>');
           $newVenueShow.append($newVenueCommentsList);
@@ -321,11 +137,12 @@ $(document).ready(function(){
           $newVenueLi.append($newVenueShow);
           $venuesList.append($newVenueLi);
         });
+        setMarkers(map, venues);
+      }
+  });
 }
-});
 
 function setMarkers(map, venues) {
-  getVenues(location);
 
   for (i = 0; i < venues.length; i++) {
 
@@ -335,23 +152,20 @@ function setMarkers(map, venues) {
         venues[i].venue.location.lng
         ),
       map: map,
-      title: dataobj[i].venue.name,
+      title: venues[i].venue.name,
       icon: 'http://i.imgur.com/rmrs7kG.png?1',
       animation:google.maps.Animation.DROP
     });
   }      
 }
-}
-});
 
+  function attachInfo(infowindow, map, marker) {
 
+    google.maps.event.addListener(marker,'click',function(){
 
-
-
-
-
-
-
-
+      infowindow.close();
+      infowindow.open(map, this);
+    });
+  }
 
 
